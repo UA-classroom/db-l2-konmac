60bdbf1107662f2acb33363d3d7f59c006642034
konrad-te <konrad-tyburcy@gmail.com>
2025-12-04 15:53:04 +0700

    Implement logic for db_setup file

--- db_setup.py
+++ db_setup.py
@@ -1,7 +1,9 @@
-import os
+import os  # Importing OS so I can read variables from my .env file where i store database login information
 
-import psycopg2
-from dotenv import load_dotenv
+import psycopg2  # Module that lets me connect to PostgreSQL database / run commands 
+from dotenv import (
+    load_dotenv,  # Reads .env file and loads the values into environment variables.
+)
 
 load_dotenv(override=True)
 
@@ -16,25 +18,35 @@ def get_connection():
     this way we'll start a new connection each time
     someone hits one of our endpoints, which isn't great for performance
     """
-    return psycopg2.connect(
-        dbname=DATABASE_NAME,
-        user="postgres",  # change if needed
-        password=PASSWORD,
-        host="localhost",  # change if needed
-        port="5432",  # change if needed
+    try:
+        connection = psycopg2.connect(
+            dbname=DATABASE_NAME,
+            user="postgres",  # change if needed
+            password=PASSWORD,
+            host="localhost",  # change if needed
+            port="5432",  # change if needed
     )
-
+        return connection
+    except Exception as e:
+        print(f"Connection failed - {e}")
 
 def create_tables():
     """
     A function to create the necessary tables for the project.
     """
     connection = get_connection()
-    # Implement
-    pass
+    if connection is None:
+        print("Unable to connect to the database")
+        return
+    with connection:
+        with connection.cursor() as cursor:
+            with open("setup.sql", "r", encoding="utf-8") as f:
+                sql = f.read()
+                cursor.execute(sql)
+    print("Tables created successfully.")
 
 
 if __name__ == "__main__":
     # Only reason to execute this file would be to create new tables, meaning it serves a migration file
     create_tables()
-    print("Tables created successfully.")
+
