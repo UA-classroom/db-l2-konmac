3251031fb98628ff4ef3463381a8f3583c7cdea3
konrad-te <konrad-tyburcy@gmail.com>
2025-12-05 15:40:08 +0700

    Error handling improvement / docstrings / info messages

--- db_setup.py
+++ db_setup.py
@@ -2,7 +2,7 @@ import os  # Importing OS so I can read variables from my .env file where i stor
 
 import psycopg2  # Module that lets me connect to PostgreSQL database / run commands 
 from dotenv import (
-    load_dotenv,  # Reads .env file and loads the values into environment variables.
+    load_dotenv,  # Reads .env file and loads the values into environment variables
 )
 
 load_dotenv(override=True)
@@ -10,43 +10,48 @@ load_dotenv(override=True)
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 PASSWORD = os.getenv("PASSWORD")
 
+if not DATABASE_NAME or not PASSWORD:
+    raise ValueError("DATABASE_NAME and PASSWORD must be set in .env file")
 
 def get_connection():
     """
-    Function that returns a single connection
-    In reality, we might use a connection pool, since
-    this way we'll start a new connection each time
-    someone hits one of our endpoints, which isn't great for performance
+    Function establishes a connection to the database
+    Uses database_name and password from env variables
+    Prints success/fail messages and returns None if connection has failed
     """
     try:
         connection = psycopg2.connect(
             dbname=DATABASE_NAME,
-            user="postgres",  # change if needed
+            user="postgres",
             password=PASSWORD,
-            host="localhost",  # change if needed
-            port="5432",  # change if needed
+            host="localhost",
+            port="5432",
     )
+        print("Database connection established successfully")
         return connection
     except Exception as e:
         print(f"Connection failed - {e}")
 
 def create_tables():
     """
-    A function to create the necessary tables for the project.
+    Creates database tables by running SQL commands from setup.sql file
+    Handles errors during table creation
     """
     connection = get_connection()
     if connection is None:
         print("Unable to connect to the database")
         return
-    with connection:
-        with connection.cursor() as cursor:
-            with open("setup.sql", "r", encoding="utf-8") as f:
-                sql = f.read()
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                with open("setup.sql", "r", encoding="utf-8") as f:
+                    sql = f.read()
                 cursor.execute(sql)
-    print("Tables created successfully.")
+            print("Tables created successfully.")
+    except Exception as e:
+            print(f"Failed to create tables - {e}")
 
 
 if __name__ == "__main__":
-    # Only reason to execute this file would be to create new tables, meaning it serves a migration file
     create_tables()
 
