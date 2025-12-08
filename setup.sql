d7f334fac89f0e21e32dbf5dd2fe37141f413ed4
konrad-te <konrad-tyburcy@gmail.com>
2025-12-08 16:22:50 +0700

    Fix minor errors

--- setup.sql
+++ setup.sql
@@ -1,125 +1,3 @@
--- CREATE TABLE IF NOT EXISTS users (
---     user_ID SERIAL PRIMARY KEY,
---     email VARCHAR(255) NOT NULL UNIQUE,
---     first_name VARCHAR(255),
---     last_name VARCHAR(255),
---     phone_number VARCHAR(255),
---     date_of_birth DATE,
---     gender VARCHAR(15) CHECK(gender IN ('Man', 'Kvinna', 'Icke-binär', 'Osäker', 'Vill ej svara', 'Annat')),
---     profile_photo VARCHAR(255),
---     balance INT NOT NULL DEFAULT 0,
---     created_at TIMESTAMP NOT NULL DEFAULT NOW()
--- );
-
--- CREATE TABLE IF NOT EXISTS business(
---     business_id SERIAL PRIMARY KEY,
---     business_name VARCHAR(50) NOT NULL,
---     email VARCHAR(255) NOT NULL UNIQUE,
---     phone_number VARCHAR(255),
---     profile_pic_url VARCHAR(255),
---     address VARCHAR(255) NOT NULL,
---     city VARCHAR(255) NOT NULL,
---     about_text TEXT,
---     number_of_employees SMALLINT
--- );
-
--- CREATE TABLE IF NOT EXISTS treatment_categories(
---     category_id SERIAL PRIMARY KEY,
---     category_name VARCHAR(255) NOT NULL UNIQUE
--- );
-
--- CREATE TABLE IF NOT EXISTS employees(
---     employee_id SERIAL PRIMARY KEY,
--- 	business_id INT NOT NULL REFERENCES business(business_id),
---     first_name VARCHAR(255),
---     last_name VARCHAR(255),
---     rating SMALLINT CHECK(rating BETWEEN 1 AND 5),
---     email VARCHAR(255) UNIQUE
--- );
-
--- CREATE TABLE IF NOT EXISTS treatments(
---     treatment_id SERIAL PRIMARY KEY,
---     business_id INT NOT NULL REFERENCES business(business_id),
---     category_id INT NOT NULL REFERENCES treatment_categories(category_id),
---     treatment_name VARCHAR(255) NOT NULL,
---     treatment_description VARCHAR(255),
---     price NUMERIC(10,2) NOT NULL,
---     time_duration INT NOT NULL,
---     last_min_deal BOOLEAN DEFAULT FALSE,
---     created_at TIMESTAMP DEFAULT NOW()
--- );
-
--- CREATE TABLE IF NOT EXISTS bookings(
---     booking_id SERIAL PRIMARY KEY,
---     treatment_id INT NOT NULL REFERENCES treatments(treatment_id),
---     user_id INT NOT NULL REFERENCES users(user_id),
---     business_id INT NOT NULL REFERENCES business(business_id),
---     employee_id INT NOT NULL REFERENCES employees(employee_id), ###########
---     booked_date TIMESTAMP NOT NULL DEFAULT NOW(),
---     booking_date TIMESTAMP NOT NULL,
---     booking_status VARCHAR(20) CHECK (booking_status IN('Bokad', 'Avbokad', 'Genomförd')),
---     created_at TIMESTAMP NOT NULL DEFAULT NOW()
--- );
-
--- CREATE TABLE IF NOT EXISTS titles(
---     title_id SERIAL PRIMARY KEY,
---     title_name VARCHAR(255) NOT NULL UNIQUE
--- );
-
--- CREATE TABLE IF NOT EXISTS employee_title(
---     employee_title_id SERIAL PRIMARY KEY,
---     employee_id INT NOT NULL REFERENCES employees(employee_id),
---     title_id INT NOT NULL REFERENCES titles(title_id)
--- );
-
--- CREATE TABLE IF NOT EXISTS payment_methods(
---     payment_method_id SERIAL PRIMARY KEY,
---     method_name VARCHAR(255) NOT NULL UNIQUE
--- );
-
--- CREATE TABLE IF NOT EXISTS payments(
---     payments_id SERIAL PRIMARY KEY,
---     user_id INT NOT NULL,
---     business_id INT NOT NULL REFERENCES business(business_id),
---     treatment_id INT NOT NULL REFERENCES treatments(treatment_id),
---     payment_method_id INT NOT NULL REFERENCES payment_methods(payment_method_id),
-    
---     payment_date TIMESTAMP DEFAULT NOW()
--- );
-
-
--- CREATE TABLE IF NOT EXISTS opening_hours(
---     opening_hour_id SERIAL PRIMARY KEY,
---     business_id INT NOT NULL REFERENCES business(business_id),
---     day_of_week INT NOT NULL CHECK(day_of_week BETWEEN 1 AND 7),
---     open_time TIME NOT NULL,
---     close_time TIME NOT NULL
--- );
-
--- CREATE TABLE IF NOT EXISTS favorites(
---     favorite_id SERIAL PRIMARY KEY,
---     treatment_id INT NOT NULL REFERENCES treatments(treatment_id),
---     user_id INT NOT NULL REFERENCES users(user_id),
---     business_id INT NOT NULL REFERENCES business(business_id),
---     created_at TIMESTAMP DEFAULT NOW(),
-
---     CONSTRAINT one_per_user UNIQUE (user_id, treatment_id)
---     --Make sure a user cant add same treatment more than once
--- );
-
--- CREATE TABLE IF NOT EXISTS reviews(
--- 	review_id SERIAL PRIMARY KEY,
--- 	user_id INT NOT NULL REFERENCES users(user_id),
--- 	employee_id INT NOT NULL REFERENCES employees(employee_id),
--- 	business_id INT NOT NULL REFERENCES business(business_id),
--- 	treatment_id INT NOT NULL REFERENCES treatments(treatment_id),
--- 	review_comment TEXT,
--- 	rating INT NOT NULL,
--- 	created_at TIMESTAMP
--- );
-
-
-
 -- DROP TABLE IF EXISTS employee_pricing CASCADE;
 -- DROP TABLE IF EXISTS employee_time_slots CASCADE;
 -- DROP TABLE IF EXISTS employee_specializations CASCADE;
@@ -167,10 +45,15 @@ CREATE TABLE users (
     last_name VARCHAR(255),
     phone_number VARCHAR(20),
     date_of_birth DATE,
-    gender VARCHAR(15),
+    gender INT NOT NULL REFERENCES gender_types(gender_id),
     profile_photo VARCHAR(200),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE gender_types (
+    gender_type_id SERIAL PRIMARY KEY,
+    gender_types VARCHAR(255)
+)
 
 CREATE TABLE businesses(
     business_id SERIAL PRIMARY KEY,
@@ -254,13 +137,10 @@ CREATE TABLE business_locations(
 
 CREATE TABLE employees(
     employee_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    business_id INT NOT NULL,
-    location_id INT NOT NULL,
-    rating SMALLINT CHECK (rating BETWEEN 1 AND 5),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (business_id) REFERENCES businesses(business_id),
-    FOREIGN KEY (location_id) REFERENCES business_locations(location_id)
+    user_id INT NOT NULL REFERENCES users(user_id),
+    business_id INT NOT NULL REFERENCES businesses(business_id),
+    location_id INT NOT NULL REFERENCES business_locations(location_id),
+    rating SMALLINT CHECK (rating BETWEEN 1 AND 5)
 );
 
 CREATE TABLE images (
@@ -430,7 +310,7 @@ CREATE TABLE business_service_categories(
 
 CREATE TABLE opening_hours(
     opening_hour_id SERIAL PRIMARY KEY,
-    location_id INT NOT NULL REFERENCES business_locations(location_id)
+    location_id INT NOT NULL REFERENCES business_locations(location_id),
     day_of_week INT NOT NULL,
     open_time TIME NOT NULL,
     close_time TIME NOT NULL
@@ -448,7 +328,7 @@ CREATE TABLE employee_specializations(
 
 CREATE TABLE employee_time_slots(
     timeslot_id SERIAL PRIMARY KEY,
-    employee_id INT NOT NULL REFERENCES employees(employee_id)
+    employee_id INT NOT NULL REFERENCES employees(employee_id),
     slot_day DATE NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
